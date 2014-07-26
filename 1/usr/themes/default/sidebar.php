d2ce92f21262552ca5f649f14673d67785250acb
leo@huaixiaoz.com <leo@huaixiaoz.com@e40ecc39-e1e6-40b0-93c5-282f4879116d>
2014-07-26 13:05:10 +0000

    fixes commit1

--- 1/usr/themes/default/sidebar.php
+++ 1/usr/themes/default/sidebar.php
@@ -7,7 +7,6 @@
             ->parse('<li><a href="{permalink}">{title}</a></li>'); ?>
         </ul>
     </section>
-    <?php endif; ?>
 
     <section class="widget">
 		<h3 class="widget-title"><?php _e('分类'); ?></h3>
@@ -17,6 +16,7 @@
         </ul>
 	</section>
     <?php endif; ?>
+
 
     <?php if (!empty($this->options->sidebarBlock) && in_array('ShowArchive', $this->options->sidebarBlock)): ?>
     <section class="widget">
