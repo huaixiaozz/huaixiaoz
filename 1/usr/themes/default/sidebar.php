d539909644e4fb9d06797e5f1f71622310b50297
leo@huaixiaoz.com <leo@huaixiaoz.com@e40ecc39-e1e6-40b0-93c5-282f4879116d>
2014-07-26 13:06:35 +0000

    refixes

--- 1/usr/themes/default/sidebar.php
+++ 1/usr/themes/default/sidebar.php
@@ -7,7 +7,21 @@
             ->parse('<li><a href="{permalink}">{title}</a></li>'); ?>
         </ul>
     </section>
+    <?php endif; ?>
 
+    <?php if (!empty($this->options->sidebarBlock) && in_array('ShowRecentComments', $this->options->sidebarBlock)): ?>
+    <section class="widget">
+		<h3 class="widget-title"><?php _e('最近回复'); ?></h3>
+        <ul class="widget-list">
+        <?php $this->widget('Widget_Comments_Recent')->to($comments); ?>
+        <?php while($comments->next()): ?>
+            <li><a href="<?php $comments->permalink(); ?>"><?php $comments->author(false); ?></a>：<?php $comments->excerpt(35, '...'); ?></li>
+        <?php endwhile; ?>
+        </ul>
+    </section>
+    <?php endif; ?>
+
+    <?php if (!empty($this->options->sidebarBlock) && in_array('ShowCategory', $this->options->sidebarBlock)): ?>
     <section class="widget">
 		<h3 class="widget-title"><?php _e('分类'); ?></h3>
         <ul class="widget-list">
@@ -16,7 +30,6 @@
         </ul>
 	</section>
     <?php endif; ?>
-
 
     <?php if (!empty($this->options->sidebarBlock) && in_array('ShowArchive', $this->options->sidebarBlock)): ?>
     <section class="widget">
@@ -44,22 +57,5 @@
         </ul>
 	</section>
     <?php endif; ?>
-
-<!-- 多说最新评论 start -->
-    <div class="ds-recent-comments" data-num-items="5" data-show-avatars="1" data-show-time="1" data-show-title="1" data-show-admin="1" data-excerpt-length="70"></div>
-<!-- 多说最新评论 end -->
-<!-- 多说公共JS代码 start (一个网页只需插入一次) -->
-<script type="text/javascript">
-var duoshuoQuery = {short_name:"huaixiaoz"};
-    (function() {
-        var ds = document.createElement('script');
-        ds.type = 'text/javascript';ds.async = true;
-        ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
-        ds.charset = 'UTF-8';
-        (document.getElementsByTagName('head')[0] 
-         || document.getElementsByTagName('body')[0]).appendChild(ds);
-    })();
-    </script>
-<!-- 多说公共JS代码 end -->
 
 </div><!-- end #sidebar -->
